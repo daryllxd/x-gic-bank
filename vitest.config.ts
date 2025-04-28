237b7f52df4a80073eb664d49aec292cba215678
Daryll Santos <daryll.santos@gmail.com>
2025-04-28 15:02:06 +0800

    Add Typescript paths

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,8 @@
 import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
-
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsConfigPaths()],
   test: {
     environment: "jsdom",
     globals: true,
